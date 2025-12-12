6a7007ee3533d0f5860785ea3cdda41f309da32a
Riyane El Qoqui <riyane.dev@gmail.com>
2025-12-12 14:33:58 +0100

    refactor(mlx): reimplement `mlx_pixel_put` and `mlx_clear_window` with pixel buffer support

--- src/clear_window.c
+++ src/clear_window.c
@@ -6,14 +6,19 @@
 /*   By: smamalig <smamalig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 17:25:26 by smamalig          #+#    #+#             */
-/*   Updated: 2025/12/12 06:57:51 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/12/12 14:31:46 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <string.h>
 
 #include "mlx.h"
 
 int	mlx_clear_window(t_mlx *mlx, t_window *win)
 {
-	XClearWindow(mlx->dpy, win->xwin);
+	(void)mlx;
+	if (!win || !win->pixel_buffer)
+		return (0);
+	memset(win->pixel_buffer, 0, win->width * win->height * sizeof(int));
 	return (0);
 }
--- src/pixel_put.c
+++ src/pixel_put.c
@@ -6,25 +6,20 @@
 /*   By: smamalig <smamalig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 03:02:28 by smamalig          #+#    #+#             */
-/*   Updated: 2025/07/01 16:10:16 by smamalig         ###   ########.fr       */
+/*   Updated: 2025/12/12 14:32:42 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
 
-int	mlx_pixel_put(t_mlx *mlx, t_window *win,
-	int x, int y, int color)
+int	mlx_pixel_put(t_mlx *mlx, t_window *win, const int x, const int y,
+		const int color)
 {
-	(void)win;
 	(void)mlx;
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0, 800, 600, 0, -1, 1);
-	glMatrixMode(GL_MODELVIEW);
-	glBegin(GL_POINTS);
-	glColor4ub((color >> 16) & 0xff, (color >> 8) & 0xff, color & 0xff,
-		(color >> 24) & 0xff);
-	glVertex2i(x, y);
-	glEnd();
+	if (!win || !win->pixel_buffer)
+		return (0);
+	if (x < 0 || x >= win->width || y < 0 || y >= win->height)
+		return (0);
+	win->pixel_buffer[(y * win->width) + x] = color;
 	return (0);
 }
