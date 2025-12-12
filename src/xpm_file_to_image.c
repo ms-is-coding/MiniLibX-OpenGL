524a9d83693bb659438e4a22d66c581b68328a1c
Riyane El Qoqui <riyane.dev@gmail.com>
2025-12-12 07:28:59 +0100

    refactor(xpm_file_to_image): restore function base implementation and cleanup includes

--- src/xpm_file_to_image.c
+++ src/xpm_file_to_image.c
@@ -6,27 +6,30 @@
 /*   By: smamalig <smamalig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 21:47:59 by smamalig          #+#    #+#             */
-/*   Updated: 2025/07/01 10:58:29 by smamalig         ###   ########.fr       */
+/*   Updated: 2025/12/12 07:26:37 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mlx.h"
 #include <fcntl.h>
 #include <stdio.h>
-#include <sys/types.h>
 #include <unistd.h>
 
-// void	*mlx_xpm_file_to_image(t_mlx *mlx, const char *filename,
-// 	int *width, int *height)
-// {
-// 	int		fd;
-// 	off_t	size;
-// 	
-// 	fd = open(filename, O_RDONLY);
-// 	if (fd == -1)
-// 		return (NULL);
-// 	size = lseek(fd, 0, SEEK_END);
-// 	if (size == -1)
-// 		return (NULL);
-// 	return (0);
-// }
+#include "mlx.h"
+
+void	*mlx_xpm_file_to_image(t_mlx *mlx, const char *filename,
+			int *width, int *height)
+{
+ 	int		fd;
+ 	off_t	size;
+
+	(void)mlx;
+	(void)width;
+	(void)height;
+	fd = open(filename, O_RDONLY);
+ 	if (fd == -1)
+ 		return (NULL);
+ 	size = lseek(fd, 0, SEEK_END);
+ 	if (size == -1)
+ 		return (NULL);
+ 	return (0);
+}
