564258b11208b980f49d0ec660f08841411b3cef
Riyane El Qoqui <riyane.dev@gmail.com>
2025-12-12 15:05:14 +0100

    refactor(destroy_window): add cleanup for texture and pixel buffer

--- src/destroy_window.c
+++ src/destroy_window.c
@@ -6,7 +6,7 @@
 /*   By: smamalig <smamalig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 20:46:02 by smamalig          #+#    #+#             */
-/*   Updated: 2025/12/12 06:57:59 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/12/12 15:04:53 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,15 @@
 
 int	mlx_destroy_window(t_mlx *mlx, t_window *win)
 {
-	(void)win;
-	XDestroyWindow(mlx->dpy, win->xwin);
-	glXDestroyContext(mlx->dpy, mlx->glc);
+	(void)mlx;
+	if (win->texture_id)
+		glDeleteTextures(1, &win->texture_id);
+	if (win->pixel_buffer)
+	{
+		free(win->pixel_buffer);
+		win->pixel_buffer = NULL;
+	}
+	if (win->xwin)
+		XDestroyWindow(mlx->dpy, win->xwin);
 	return (0);
 }
