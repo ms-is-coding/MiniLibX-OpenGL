b375b8cfff51ee1d76da665a8ede19c70969ecbe
Riyane El Qoqui <riyane.dev@gmail.com>
2025-12-21 12:11:08 +0100

    feat(mlx): extend `mlx_xpm_to_image` to support 2-byte color parsing with LUT

--- src/xpm_to_image.c
+++ src/xpm_to_image.c
@@ -6,14 +6,17 @@
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/20 14:07:42 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/12/20 20:30:10 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/12/21 12:09:51 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "mlx.h"
+
+#define CPP2_LUT_SIZE 65536
 
 typedef struct s_xpm_ctx {
 	char		**data;
@@ -23,6 +26,7 @@ typedef struct s_xpm_ctx {
 	int			num_col;
 	int			cpp;
 	uint32_t	fast_lut[256];
+	uint32_t	*big_lut;
 }	t_xpm_ctx;
 
 static uint32_t	parse_color(const char *str)
@@ -39,7 +43,7 @@ static uint32_t	parse_color(const char *str)
 		str++;
 	if (strncasecmp(str, "None", 4) == 0)
 		return (0x00000000);
-	if (sscanf(str, "%02x%02x%02x", &r, &g, &b) == 3)
+	if (sscanf(str, "%2x%2x%2x", &r, &g, &b) == 3)
 		return (0xFF000000 | (r << 16) | (g << 8) | b);
 	return (0xFF000000);
 }
@@ -50,6 +54,7 @@ static int	build_color_table(t_xpm_ctx *ctx)
 	char		*line;
 	char		*col_start;
 	uint32_t	color_val;
+	int			idx;
 
 	if (ctx->cpp > 2)
 		return (0);
@@ -63,6 +68,12 @@ static int	build_color_table(t_xpm_ctx *ctx)
 		color_val = parse_color(col_start + 2);
 		if (ctx->cpp == 1)
 			ctx->fast_lut[(unsigned char)line[0]] = color_val;
+		if (ctx->cpp == 2)
+		{
+			idx = ((unsigned char)line[0] << 8) | (unsigned char)line[1];
+			if (idx < CPP2_LUT_SIZE)
+			ctx->big_lut[idx] = color_val;
+		}
 		i++;
 	}
 	return (1);
@@ -74,6 +85,7 @@ static void	fill_texture(t_xpm_ctx *ctx, int *buffer)
 	int		x;
 	int		y;
 	char	*line;
+	int		idx;
 
 	data_offset = 1 + ctx->num_col;
 	y = 0;
@@ -85,8 +97,12 @@ static void	fill_texture(t_xpm_ctx *ctx, int *buffer)
 		{
 			if (ctx->cpp == 1)
 				buffer[y * ctx->w + x] = ctx->fast_lut[(unsigned int)line[x]];
-			else
-				buffer[y * ctx->w + x] = 0xFFFF00FF;
+			else if (ctx->cpp == 2)
+			{
+				idx = ((unsigned char)line[x * 2] << 8)
+					| (unsigned char)line[x * 2 + 1];
+				buffer[y * ctx->w + x] = ctx->big_lut[idx];
+			}
 			x++;
 		}
 		y++;
@@ -100,19 +116,34 @@ void	*mlx_xpm_to_image(t_mlx *mlx, char **xpm_data, int *width, int *height)
 
 	if (!mlx || !xpm_data)
 		return (NULL);
+	memset(&ctx, 0, sizeof(t_xpm_ctx));
 	if (sscanf(xpm_data[0], "%d %d %d %d", &ctx.w, &ctx.h, &ctx.num_col,
 			&ctx.cpp) != 4)
 		return (NULL);
 	ctx.data = xpm_data;
+	if (ctx.cpp == 2)
+	{
+		ctx.big_lut = calloc(CPP2_LUT_SIZE, sizeof(uint32_t));
+		if (!ctx.big_lut)
+			return (NULL);
+	}
 	img = mlx_new_image(mlx, ctx.w, ctx.h);
 	if (!img)
+	{
+		if (ctx.big_lut)
+			free(ctx.big_lut);
 		return (NULL);
+	}
 	if (!build_color_table(&ctx))
 	{
 		mlx_destroy_image(mlx, img);
+		if (ctx.big_lut)
+			free(ctx.big_lut);
 		return (NULL);
 	}
 	fill_texture(&ctx, img->buffer);
+	if (ctx.big_lut)
+		free(ctx.big_lut);
 	if (width)
 		*width = ctx.w;
 	if (height)
