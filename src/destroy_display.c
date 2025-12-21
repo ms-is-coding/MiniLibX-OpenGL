b83e4d2a0e4dfa2e20be72e458316b8a165c0b76
Riyane El Qoqui <riyane.dev@gmail.com>
2025-12-21 12:31:07 +0100

    refactor(mlx): extract X resource cleanup logic into a helper function

--- src/destroy_display.c
+++ src/destroy_display.c
@@ -6,19 +6,35 @@
 /*   By: smamalig <smamalig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 20:16:23 by smamalig          #+#    #+#             */
-/*   Updated: 2025/12/20 15:09:17 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/12/21 12:17:19 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
 
-int	mlx_destroy_display(t_mlx *mlx)
+__attribute__((always_inline))
+static inline void	clear_x_resources(t_mlx *mlx)
 {
+	if (mlx->gc)
+	{
+		XFreeGC(mlx->dpy, mlx->gc);
+		mlx->gc = NULL;
+	}
+	if (mlx->vi)
+	{
+		XFree(mlx->vi);
+		mlx->vi = NULL;
+	}
 	if (mlx->font)
 	{
 		XFreeFont(mlx->dpy, mlx->font);
 		mlx->font = NULL;
 	}
+}
+
+int	mlx_destroy_display(t_mlx *mlx)
+{
+	clear_x_resources(mlx);
 	if (mlx->glc)
 	{
 		glXDestroyContext(mlx->dpy, mlx->glc);
